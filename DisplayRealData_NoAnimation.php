2ed911a5169ad8cc17a49569233bbb1bcbc5391b
Max Xu <yifax10@uci.edu>
2018-07-06 14:17:31 -0700

    V 0.1
    Initial Commit
    Working Code

---
+++ DisplayRealData_NoAnimation.php
@@ -0,0 +1,85 @@
+<?php /* Template Name: Data_Display V1.0 Real data, no animation */ ?>
+<?php /* NOTE: Google Chart API loaded at header.php */ ?>
+
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+			// Include the page content template.
+			// get_template_part( 'content', 'page' );
+			// If comments are open or we have at least one comment, load up the comment template.
+			// if ( comments_open() || get_comments_number() ) :
+			// comments_template();
+			//endif;
+		endwhile;
+
+
+		?>
+		<?php
+		global $wpdb;
+		$records = $wpdb->get_results("SELECT * FROM C2Ctest;");	// Acquire data from Table C2Ctest
+		$records_json = wp_json_encode($records);
+		//echo $records_json;
+		//print_r($records);
+		?>
+
+		<script>
+    		var json = <?php echo $records_json; ?>; 	//Don't forget the extra semicolon!
+    	</script>
+
+
+    	<?php echo "
+    	<script>
+    		google.charts.load('current', {packages: ['corechart', 'line']});
+			google.charts.setOnLoadCallback(drawBasic);
+			function drawBasic() {
+				
+				var data = new google.visualization.DataTable();
+				data.addColumn('string', 'Date');
+				data.addColumn('number', 'Viewers');
+				
+				for (var i = 0; i < json.length; i++) {
+					//console.log(typeof(json[i]['WebSiteViews']));
+					data.addRow([json[i]['Date'], Number(json[i]['WebSiteViews'])]);
+					
+				}
+
+				var options = {
+					hAxis: {
+					title: 'Date'
+					},
+					vAxis: {
+					title: 'Count'
+					}
+				};
+				
+				var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+				chart.draw(data, options);
+		    }
+		</script>
+		"
+		?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+	<div id="chart_div"></div>
+
+<?php get_footer(); ?>
+
